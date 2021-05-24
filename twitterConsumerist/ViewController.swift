9241e07c16e714073870cef4801d60fa030ac2d5
Martín Granados García <martin.granados@wizeline.com>
2021-05-23 19:56:24 -0500

    Initial Commit

---
+++ twitterConsumerist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  twitterConsumerist
+//
+//  Created by Martin Granados Garcia on 5/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
