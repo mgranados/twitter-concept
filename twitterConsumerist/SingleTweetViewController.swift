6678467dabaa6aa36b9a18807eeafe1e83048517
Martín Granados García <martin.granados@wizeline.com>
2021-06-22 09:10:38 -0500

    Add upping the view when keyboard appears on single tweet

--- twitterConsumerist/SingleTweetViewController.swift
+++ twitterConsumerist/SingleTweetViewController.swift
@@ -12,6 +12,7 @@ class SingleTweetViewController: UIViewController {
     var username: String?
     var tweetText: String?
 
+    @IBOutlet var tweetInput: UITextField!
     @IBOutlet var profileImage: UIImageView!
     @IBOutlet var usernameLabel: UILabel!
     @IBOutlet var tweetTextLabel: UILabel!
@@ -21,5 +22,27 @@ class SingleTweetViewController: UIViewController {
         profileImage.image = profilePic
         usernameLabel.text = username
         tweetTextLabel.text = tweetText
+
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillAppear), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillDisappear), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
+
+    @objc func keyboardWillAppear(notification: NSNotification) {
+        guard let userInfo = notification.userInfo else {
+            return
+        }
+        guard let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else {
+            return
+        }
+        if self.view.frame.origin.y <= 0 {
+            self.view.frame.origin.y = -keyboardFrame.cgRectValue.height
+        }
+    }
+
+    @objc func keyboardWillDisappear(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+
 }
